72b6f3b8f3e4b4fdb7718a0369e03ba7f6564a63
Rudy PLESSIS <rudy.plessis@gmail.com>
2023-07-03 16:20:56 +0200

    Express Quest 6

--- userHandlers.js
+++ userHandlers.js
@@ -62,43 +62,48 @@ const postUser = (req, res) => {
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
-  const {firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email, city, language } = req.body;
 
-  database.query("UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?", [firstname, lastname, email, city, language])
-  .then(([result]) => {
-    if (result.affectedRows === 0) {
-      res.status(404).send("Not Found");
-    } else {
-      res.sendStatus(204);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error editing the user");
-  })
-}
+  database
+    .query(
+      "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
 
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
 
-  database.query("DELETE FROM users WHERE id = ?", [id])
-  .then(([result]) => {
-    if(result.affectedRows === 0) {
-      res.status(404).send("Not Found");
-    } else {
-      res.sendStatus(204);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error deleting the user");
-  });
-}
+  database
+    .query("DELETE FROM users WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
 
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
-  deleteUser, 
+  deleteUser,
 };
